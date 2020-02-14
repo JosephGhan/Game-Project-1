5116d83550e644823c5741beaa9668b99874b2cd
JosephGhan <47993853+JosephGhan@users.noreply.github.com>
2020-02-13 20:51:04 -0600

    Added HighScore

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -6,12 +6,32 @@ using UnityEngine.UI;
 
 public class Timer : MonoBehaviour
 {
-    
+    [Header("Set it boi")]
+    static public int score = 100;
+    public Text gt;
+
+    void Awake()
+    {
+        if (PlayerPrefs.HasKey("HighScore"))
+        {
+            score = PlayerPrefs.GetInt("HighScore");
+        }
+
+        PlayerPrefs.SetInt("HighScore", score);
+    }
 
     // Update is called once per frame
     void Update()
     {
+        gt.text = "High Score: " + score;
         
+        if (PlayerController.count == 12)
+        {
+            if (score < PlayerPrefs.GetInt("HighScore"))
+            {
+                PlayerPrefs.SetInt("HighScore", score);
+            }
+        }
         
     }
 }
